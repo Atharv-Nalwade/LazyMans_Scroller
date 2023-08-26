2ee18000a1e6eb4f18482a2966ea3cd80edc9a2d
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-08-26 23:26:36 +0530

    refactor:Added functions to stop the script on browser window closure

--- main.py
+++ main.py
@@ -1,29 +1,49 @@
-# Python program to demonstrate selenium
-
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException, NoSuchWindowException
 import time
+import keyboard  # Import the keyboard library
 
 
-driver = webdriver.Firefox()
-driver.get("https://www.youtube.com/")
-
-print(driver.title)
-
-time.sleep(10)
-
-shorts_tab = driver.find_element(
-    "xpath", "/html/body/ytd-app/div[1]/ytd-mini-guide-renderer/div/ytd-mini-guide-entry-renderer[2]/a/yt-icon/yt-icon-shape/icon-shape/div")
-shorts_tab.click()
-
-time.sleep(20)
-
-while True:
-    shorts_screen = driver.find_element(
-        "xpath", '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-shorts/div[4]/div[2]/ytd-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div')
-    shorts_screen.click()
-    time.sleep(30)
+def create_driver():
+    return webdriver.Firefox()
 
 
-driver.quit()
+def is_driver_alive(driver):
+    try:
+        driver.title
+        return True
+    except (WebDriverException, NoSuchWindowException):
+        return False
+
+
+def main():
+    driver = create_driver()
+    driver.get("https://www.youtube.com/")
+
+    print(driver.title)
+
+    time.sleep(10)
+
+    shorts_tab = driver.find_element(
+        "xpath", "/html/body/ytd-app/div[1]/ytd-mini-guide-renderer/div/ytd-mini-guide-entry-renderer[2]/a/yt-icon/yt-icon-shape/icon-shape/div")
+    shorts_tab.click()
+
+    time.sleep(20)
+
+    try:
+        while is_driver_alive(driver):
+            shorts_screen = driver.find_element(
+                "xpath", '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-shorts/div[4]/div[2]/ytd-button-renderer/yt-button-shape/button/yt-touch-feedback-shape/div')
+            shorts_screen.click()
+            time.sleep(30)
+
+    except KeyboardInterrupt:
+        print("Exiting...")
+    finally:
+        driver.quit()
+
+
+if __name__ == "__main__":
+    main()
